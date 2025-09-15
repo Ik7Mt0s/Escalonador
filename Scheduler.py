d6237f57d529b5adb99343ba8d40a828569ef2e8
Ik7Mt0s <icaromatoscb@gmail.com>
2025-09-15 10:01:06 -0300

    classe Processo

---
+++ Scheduler.py
@@ -0,0 +1,10 @@
+class Processo():
+    def __init__(self, id, nome, prioridade, ciclos_necessarios, recurso_necessario):
+        self.id = id
+        self.nome = nome
+        self.prioridade = prioridade
+        self.ciclos_necessarios = ciclos_necessarios
+        self.recurso_necessario = recurso_necessario
+
+    def __str__(self):
+        return f"{self.nome} (ID: {self.id}, Prioridade: {self.prioridade})"
